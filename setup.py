e424e1c02e6156d35b1efd78185fef3fbdf56c4f
Marco Stadler <marco.stadler@jku.at>
2023-05-08 17:16:22 +0200

    renamed statemachine package
    Signed-off-by: Marco Stadler <marco.stadler@jku.at>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['admon_mode_cps_pkg'],
+    packages=['admon_mode_cps_pkg', 'python_statemachine-2.0.0'],
     package_dir={'': 'src'}
 )
 setup(**d)
